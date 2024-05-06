d183c1499c7b7c2228210fbbc9545f42b023246e
TerminalVibes <87617218+TerminalVibes@users.noreply.github.com>
2024-05-06 16:51:36 -0400

    Update test-ap.lua

--- tests/test-ap.lua
+++ tests/test-ap.lua
@@ -4,41 +4,16 @@ local Workspace = game:GetService("Workspace")
 
 local LocalPlayer = Players.LocalPlayer
 
-local acharacter = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
-local aroot = acharacter:WaitForChild("HumanoidRootPart")
+local character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
+local root = character:WaitForChild("HumanoidRootPart")
+local humanoid = character:WaitForChild("Humanoid")
+local animator = humanoid:WaitForChild("Animator")
 
-local combo = 1
-acharacter:GetAttributeChangedSignal("LastM1Fire"):Connect(function()
-	warn(combo - 1)
-	local t = tick()
-	local c =acharacter:GetAttributeChangedSignal("LastDamageDone"):Once(function()
-		print((tick() - t) * 1000)
+character:GetAttributeChangedSignal("LastDashSwing"):Connect(function()
+	local s = os.clock()
+	character:GetAttributeChangedSignal("LastDamageDone"):Once(function()
+		local t = os.clock()
+		local dt = t - s
+		print(`Damage took ${dt * 1000} ms`)
 	end)
-	task.wait(0.5)
-	c:Disconnect()
 end)
-acharacter:GetAttributeChangedSignal("Combo"):Connect(function()
-	local currentCombo = acharacter:GetAttribute("Combo")
-	combo = currentCombo
-end)
-
-acharacter:GetAttributeChangedSignal("LastDashSwing"):Connect(function()
-	local t = tick()
-	local c =acharacter:GetAttributeChangedSignal("LastDamageDone"):Once(function()
-		print((tick() - t) * 1000)
-	end)
-	task.wait(2)
-	c:Disconnect()
-end)
-
-
-local plr = game.Players.LocalPlayer
-plr:GetAttributeChangedSignal("Character"):Once(function()
-	print("Character changed")
-end)
-plr.CharacterAdded:Once(function()
-	print("Character added")
-end)
-
-
-
